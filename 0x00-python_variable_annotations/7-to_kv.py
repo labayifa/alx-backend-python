0ed49cc8c138e3891d3135deb5cb972d1739f109
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-10 20:12:09 +0100

    Union data

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""
+7. Complex types - string and int/float to tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    :param k:
+    :param v:
+    :return: tuple with string value of k and square value of v
+    """
+    return (k, v**2)
