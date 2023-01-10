44fe8daba085bfe5ece9a219b40dcdad73e886b5
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-10 19:41:08 +0100

    Python annotationa

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+3. Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    :param n:
+    :return: string representation of n
+    """
+    return "{}".format(n)
