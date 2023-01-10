00d5711a08e2fec776f2d8b0023284090d7b50fc
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-10 20:19:23 +0100

    8. Complex types - functions

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+8. Complex types - functions
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    :param multiplier:
+    :return: Returns a function that multiplies a float.
+    """
+    return lambda x: x * multiplier
