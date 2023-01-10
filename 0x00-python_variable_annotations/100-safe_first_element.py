8e285b4a3c007ba91871fef35290c2d401d10044
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-10 20:33:41 +0100

    100 Any Type

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+
+"""
+from typing import Any, Union, Sequence
+
+
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    :param lst:
+    :return:
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
