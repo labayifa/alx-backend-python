3fdbdff49b5d0e1385676fada48335558a42f65d
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-10 19:59:02 +0100

    Union data

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+6. Complex types - mixed list
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    :param mxd_lst:
+    :return: sum of mixed type of value from list
+    """
+    return sum(mxd_lst)
