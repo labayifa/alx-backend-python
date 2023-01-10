87c6f88544b173464e1c64d1f5582c703956ee93
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-10 19:50:56 +0100

    Sum list content.

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+5. Complex types - list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    :param input_list:
+    :return: sum of float value contained in input_list
+    """
+    return sum(input_list)
