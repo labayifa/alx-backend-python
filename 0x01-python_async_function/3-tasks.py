67435ba84a7b19768d05b70a946856c5041e32e2
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-11 01:50:50 +0100

    0. The basics of async

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+3. Tasks
+"""
+import asyncio
+from asyncio import Task
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """
+    :param max_delay:
+    :return:
+    """
+    return asyncio.create_task(wait_random(max_delay))
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+4. Tasks
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    :param n:
+    :param max_delay:
+    :return: return the ordered list following time used
+             to complete each task from tas list
+    """
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
