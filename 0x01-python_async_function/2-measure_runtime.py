43292c257ea931810c9449e2b50021a1387d424c
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-11 01:40:28 +0100

    0. The basics of async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+2. Measure the runtime
+"""
+import asyncio
+from time import perf_counter
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    :param n:
+    :param max_delay:
+    :return:
+    """
+    start = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    return (perf_counter() - start) / n
