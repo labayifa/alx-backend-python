e7eefb83d123fff0cc2d6a604e504553248f4f75
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-11 01:19:34 +0100

    0. The basics of async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+1. Let's execute multiple coroutines at the same time with async
+"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    :param n:
+    :param max_delay:
+    :return: return the ordered list following time used to complete each task from tas list
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
