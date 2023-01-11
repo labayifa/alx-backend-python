dd4c1da53b9e15aa1df9b5a14827867fd8249a0b
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-11 01:21:02 +0100

    0. The basics of async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,8 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     :param n:
     :param max_delay:
-    :return: return the ordered list following time used to complete each task from tas list
+    :return: return the ordered list following time used
+             to complete each task from tas list
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
